3e9b96642a5f6d2ed4aba150874bc8feef23a57b
Nathaniel Edwards <nedwards@gmail.com>
2016-08-28 12:49:56 -0700

    Clean up code and remove old nodes from svg after 10 seconds

--- force.js
+++ force.js
@@ -1,92 +1,53 @@
 function init() {
-  // var svg = d3.select("svg");
 
+  var svg = d3.select("svg");
+  var width = +svg.attr("width");
+  var height = +svg.attr("height");
+  var count = 0;
+  var nodes = [];
+  var charging = false;
 
-  // // function force(alpha) {
-  // //   nodes = sim.nodes();
-  // //   for (var i = 0, n = nodes.length, node, k = alpha * 0.1; i < n; ++i) {
-  // //     node = nodes[i];
-  // //     node.vx -= node.x * k;
-  // //     node.vy -= node.y * k;
-  // //   }
-  // // }
-
-  // var nodes = [
-  //   {"id": "Myriel", "group": 1},
-  //   {"id": "Napoleon", "group": 1},
-  //   {"id": "Mlle.Baptistine", "group": 1},
-  //   {"id": "Mme.Magloire", "group": 1},
-  // ];
-
-  // var sim = d3.forceSimulation(nodes)
-  //   // .force("charge", d3.forceManyBody())
-  //   // .force("link", d3.forceLink().id(function(d) { return d.id; }))
-  //   .force("center", d3.forceCenter([300, 300]));
-
-  // var node = svg.append("g")
-  //   .attr("class", "nodes")
-  //   .selectAll("circle")
-  //   .data(nodes)
-  //   .enter().append("circle")
-  //     .attr("r", 5)
-  //     .attr("fill", "gold");
-  
-  // sim
-  //   .on('tick', ticked)
-  //   .on('end', () => console.log('end'));
-
-  // function ticked() {
-  //   node
-  //     .attr("cx", function(d) { return d.x; })
-  //     .attr("cy", function(d) { return d.y; });
-  // }
-
-var svg = d3.select("svg");
-var width = +svg.attr("width");
-var height = +svg.attr("height");
-
-var nodes = d3.range(50).map((_, i) => ({id: i, x: width * Math.random(), y: -50 * Math.random()}));
-var count = nodes.length;
-
-var simulation = d3.forceSimulation()
-    .force("charge", d3.forceManyBody().strength(() => 0.5))
-    .force("yposition", d3.forceY(height).strength(() => 0.03))
-    .force("xposition", d3.forceX(width / 2).strength(() => 0.005))
-    .alpha(0.1)
+  var simulation = d3.forceSimulation()
+    // .force("charge", d3.forceManyBody().strength(() => 0.5))
+    .force("yposition", d3.forceY(height - 10).strength(() => 0.005))
+    .force("xposition", d3.forceX(width / 2).strength(() => charging ? 0.005 : 0))
+    .alpha(0.01)
     .alphaDecay(0)
     // alphaDecay is supposed to be between [0, 1]...
-    // I wanted acceleration though, so we'll need to remove nodes fast
-    .velocityDecay(0.6);
+    // but to have acceleration, you need to have negative alpha?
+    // but then this acceleration applies to new nodes too – not worth it
+    // ***probably best to define your own force, with increasing attraction as you get closer to a point***
+    .velocityDecay(0.01);
 
+  function generateNodes(n) {
+    count += n;
+    return d3.range(n).map((_, i) => ({id: i, x: width * Math.random(), y: -50 * Math.random()}));
+  }
 
-  var drawNodes = (nodes) => { 
+  function drawNodes(nodes) { 
     return svg.append("g")
       .attr("class", "nodes")
       .selectAll("circle")
       .data(nodes)
       .enter().append("circle")
         .attr("r", 5)
-        .attr("fill", "gold");
-  };
-
-  var node = drawNodes(nodes);
-
-  simulation
-      .nodes(nodes)
-      .on("tick", ticked);
+        .attr("fill", "gold")
+      .transition()
+        .duration(10000)
+      .remove();
+  }
 
   function ticked() {
-    node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
+    nodeElements
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
   }
 
   setInterval(() => {
-    var nodes2 = d3.range(20).map((_, i) => ({id: count + i, x: width * Math.random(), y: -50 * Math.random()}));
-    count += 50;
-    drawNodes(nodes2);
-    node = d3.selectAll("circle");
-    nodes = nodes.concat(nodes2);
+    var newNodes = generateNodes(5);
+    drawNodes(newNodes);
+    nodeElements = d3.selectAll("circle");
+    nodes = nodes.concat(newNodes);
     simulation.nodes(nodes).on("tick", ticked);
   }, 500);
 }
